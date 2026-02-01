88d986c737f705127c4ee72fc97eb5066a49d823
SadieSin <sarahnielsen2008@gmail.com>
2026-02-01 19:48:56 +0100

    Update protected.lua

--- protected.lua
+++ protected.lua
@@ -1,2 +1,2 @@
--- SuperFlingX - Protected Loader (fixed filename)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SadieSin/SuperFlingX/main/obf_3e06O80sC5OfG6aqUe3yxW6GUS5V1NUsgDgm1PYR3wF1rJPu7BslyMPwN4f48b25.lua"))()
+-- SuperFlingX Protected Loader
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SadieSin/SuperFlingX-Loader/main/SuperFlingXLoader.lua"))()
