2d81754dcc11efbe61f3934e573b06bc4ceb87f6
SadieSin <sarahnielsen2008@gmail.com>
2026-02-01 19:58:22 +0100

    Update SuperFlingXLoader.lua

--- SuperFlingXLoader.lua
+++ SuperFlingXLoader.lua
@@ -1,1 +1,6 @@
-g
+-- SuperFlingX Middle Loader (hidden layer)
+local main = "https://raw.githubusercontent.com/SadieSin/SuperFlingX/main/obf_3e06O80sC5OfG6aqUe3yxW6GUS5V1NUsgDgm1PYR3wF1rJPu7BslyMPwN4f48b25.lua"
+
+pcall(function()
+    loadstring(game:HttpGet(main, true))()
+end)
